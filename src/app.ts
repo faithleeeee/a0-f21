63e94afb2e1a8814a7f06054d6299e66368b6ef6
Faith Li <74921208+faithleeeee@users.noreply.github.com>
2021-09-01 23:55:57 -0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -101,23 +101,82 @@ class Drawing {
         // if the mouse is over the canvas, it's position is here, otherwise it's null
         // add code to deal with the mouse position each render frame
         if (this.mousePosition) {
-
+            //this.ctx.fillStyle = this.rectColor.string();
+            this.points.addPoint(this.mousePosition)
         } else {
-
+            if(this.points.length > 0){
+                this.points.dropPoint();
+            }
         }
 
         // add code to draw rectangles we have so far at the back
+         this.rects.forEach(element =>{
+            var container = this.ctx.lineWidth
+            this.ctx.lineWidth = 5
+            var width = -element.p1.x + element.p2.x
+            var height = -element.p1.y + element.p2.y
+            this.ctx.strokeRect(element.p1.x, element.p1.y,width, height);
+            this.ctx.beginPath();
+            this.ctx.moveTo(element.p1.x, element.p1.y);
+            this.ctx.lineTo(element.p2.x, element.p2.y);
+            this.ctx.moveTo(element.p1.x + width, element.p1.y);
+            this.ctx.lineTo(element.p2.x - width, element.p2.y);
+            this.ctx.stroke();
+            this.ctx.closePath();
+            this.ctx.lineWidth = container
+            var centerP: ps.MousePosition = {x: element.p1.x + width/2, 
+                y: element.p1.y + height/2};
+            var p1 = element.p1
+            var p2: ps.MousePosition = {x: p1.x + width, y: p1.y}
+            var p3: ps.MousePosition = {x: p2.x, y: p2.y + height}
+            var p4: ps.MousePosition = {x: p1.x, y: p1.y + height}
+            var smaller = Math.min(Math.abs(width), Math.abs(height));
+            var step = Math.floor(smaller/128)
+            var check = smaller%128
+            if(check == 0){
+                step = step -1;
+            }
+            Triangle(centerP, p1, p2, element.color, this.ctx, step);
+            Triangle(centerP, p2, p3, element.color, this.ctx, step);
+            Triangle(centerP, p3, p4, element.color, this.ctx, step);
+            Triangle(centerP, p4, p1, element.color, this.ctx, step);
+
+
+
+         });
+
         
 
+         
 
-        // add code to draw points with the oldest ones more transparent 
-
+        // add code to draw points with the oldest ones more transparent  
+        var pointColor = darken(Color('blue'))
+        this.ctx.fillStyle = pointColor.toString();
+        for(let index = this.points.length -1; index >= 0; index --){
+            var element = this.points.getPoint(index);
+            this.ctx.fillRect(element.x, element.y, 5, 5);
+            this.ctx.fillStyle = pointColor.fade(.7).toString()
+        }
         
-
         // if we've clicked, add code draw the rubber band
         if (this.clickStart) {
-
+            if(this.mousePosition != null){
+                var original = this.ctx.strokeStyle
+                var startx = this.clickStart.x
+                var starty = this.clickStart.y
+                this.ctx.strokeStyle = 'grey'
+                this.ctx.strokeRect(this.clickStart.x, this.clickStart.y,
+                    -this.clickStart.x + this.mousePosition.x, -this.clickStart.y + this.mousePosition.y)
+                    this.ctx.strokeStyle = original
+            } else {
+                this.clickStart = null;
+            }
+            
         }
+
+
+        
+
 
         // do it again!  and again!  AND AGAIN!  AND ...       
         requestAnimationFrame(() => this.render());
@@ -147,12 +206,20 @@ class Drawing {
 
             // **** TODO *****
             // add code here to react to mouse up events
+            if(this.clickStart != null){
+                let rect: Rectangle = {p1 : this.clickStart, p2 : clickEnd, color: randomColor()};
+                this.rects.push(rect);
+                this.clickStart = null; // start another rect
+            }
+            this.mousePosition = clickEnd;
+
         }
         
         canv.onmousemove = (ev: MouseEvent) => {
             // this method is called when the mouse moves.   
             const mouse = Drawing.offset(ev);
             this.mousePosition = mouse 
+            
         }
         
         canv.onmouseout = (ev: MouseEvent) => {
@@ -209,6 +276,35 @@ function exec() {
     
     // kick off the rendering!
     myDrawing.render(); 
+}
+function Triangle(center: ps.MousePosition , p1: ps.MousePosition, p2: ps.MousePosition, 
+    color: Color, ctx: CanvasRenderingContext2D, step: number ){
+
+        var midP1: ps.MousePosition = {x: (p1.x - center.x)/2 + center.x, y: (p1.y - center.y)/2 + center.y};
+        var midP2: ps.MousePosition = {x: (p2.x - center.x)/2 + center.x, y: (p2.y - center.y)/2 + center.y};
+        var midP3: ps.MousePosition = {x: (p2.x - p1.x)/2 + p1.x, y: (p2.y - p1.y)/2 + p1.y};
+        var original = ctx.fillStyle
+        ctx.fillStyle = color.toString()
+
+        ctx.beginPath()
+
+        ctx.strokeStyle = color.toString();
+        ctx.lineWidth = 5;
+
+        ctx.beginPath();
+        ctx.moveTo(midP1.x, midP1.y);
+        ctx.lineTo(midP1.x, midP1.y);
+        ctx.lineTo(midP2.x, midP2.y);
+        ctx.lineTo(midP3.x, midP3.y);
+        ctx.stroke();
+        ctx.fill()
+        ctx.closePath()
+        if(step > 0){
+            Triangle(center, midP1, midP2, darken(color), ctx, step-1);
+            Triangle(midP1, p1, midP3, darken(color), ctx, step-1);
+            Triangle(midP2, midP3, p2, darken(color), ctx, step-1);
+        }
+
 }
 
 exec()
